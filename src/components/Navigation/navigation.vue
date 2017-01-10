34efb3c632a8c7d2f03fd867414919e3ed39b8a1
Matt <matt@saowapan.com>
2017-01-10 04:39:56 +0000

    just some testing with vue-material, seems okay now

--- src/components/Navigation/navigation.vue
+++ src/components/Navigation/navigation.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="navigation">
-    <h1>navigation</h1>
-    <h2>{{ msg }}<h2>
-  </div>
+  <md-toolbar class="navigation">
+    <h2 class="md-title">Tracking Dashboard</h2>
+  </md-button>
+
 </template>
 
 <script>
@@ -18,5 +18,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .navigation {
+    width: 100%;
+  }
 </style>
