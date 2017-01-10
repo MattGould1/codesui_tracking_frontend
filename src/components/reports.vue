cdfa21884906bbc2fee5a5fe6efd55de57e0d336
Matt <matt@saowapan.com>
2017-01-10 04:40:26 +0000

    placeholder for graphs

---
+++ src/components/reports.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="reports">
+    <h1>My awesome reports!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reports',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
